b5cbca0f862887736282604708159288f21e9552
vishwaoffl <rvishwavinoth@gmail.com>
2023-08-04 03:09:13 +0530

    .

---
+++ index.js
@@ -0,0 +1,60 @@
+// This is a single line command
+
+/*  This is a 
+    multiple line 
+    command 
+*/
+
+// #1 Echoing JavaScript
+
+// // Alerting
+// alert("Happy Hacking!");
+
+// // Logging 
+// console.log(alert);
+
+// //  Addition
+// alert(4 * 3);
+// console.log(alert);
+
+/* 
+    Variable declaration
+    var - is a Old Way Of saving the variable 
+    let - Its a Modern way of saving the variable 
+    const 
+*/
+
+// let myName = "vishwa";
+// let myMsg = "Hello my name is " + myName + ", I'm Learning JavaScript";
+// // alert(msg);
+// console.log(myMsg);
+
+// let number1 = 30;
+// let number2 = 12;
+// let result = number1 * number2;
+// console.log(result);
+// // alert(result);
+
+// const myCode = "thv07"
+// let myCodeName = "My code name is " + myCode + "." 
+// console.log(myCodeName);
+
+let myFullName = "vishwa"; // String 
+let myAge = 20; // Integer & Number
+let isYoung = true; // Boolean
+let rightNow = undefined; // Undefined
+let goal = null; // Null
+// goal = "unconditional";
+
+/* 
+    Rules:
+    1. No JS keywords name;
+    2. Should not start with Numbers and Uppercase;
+    3. Its Case-Sensitive ( myFunction );
+    4. No space & No - , _ ;
+    5. Use Meaning Full names;
+*/
+
+
+
+
