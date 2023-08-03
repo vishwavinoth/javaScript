fecaf8def839950ffcf82652bdc883ba4bb58292
vishwaoffl <rvishwavinoth@gmail.com>
2023-08-04 03:11:43 +0530

    -

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 // // Alerting
 // alert("Happy Hacking!");
 
-// // Logging 
+// // Logging
 // console.log(alert);
 
 // //  Addition
@@ -36,15 +36,15 @@
 // // alert(result);
 
 // const myCode = "thv07"
-// let myCodeName = "My code name is " + myCode + "." 
+// let myCodeName = "My code name is " + myCode + "."
 // console.log(myCodeName);
 
-let myFullName = "vishwa"; // String 
-let myAge = 20; // Integer & Number
-let isYoung = true; // Boolean
-let rightNow = undefined; // Undefined
-let goal = null; // Null
-// goal = "unconditional";
+// let myFullName = "vishwa"; // String
+// let myAge = 20; // Integer & Number
+// let isYoung = true; // Boolean
+// let rightNow = undefined; // Undefined
+// let goal = null; // Null
+// // goal = "unconditional";
 
 /* 
     Rules:
@@ -55,6 +55,111 @@ let goal = null; // Null
     5. Use Meaning Full names;
 */
 
+// Function to create the "javaScript" div element with loading effect
+function createJavaScriptElement(textContent) {
+  // Create a new <div> element with class "javaScript"
+  const divElement = document.createElement("div");
+  divElement.classList.add("javaScript", "javaScript-loading");
 
+  // Create a new <h1> element with the specified text content
+  const h1Element = document.createElement("h1");
+  h1Element.textContent = textContent;
 
+  // Append the <h1> element as a child to the <div> element
+  divElement.appendChild(h1Element);
 
+  // Simulate the loading time with a delay of 1000ms (1 second)
+  setTimeout(() => {
+    // Replace the "javaScript-loading" class with "javaScript-loaded"
+    divElement.classList.remove("javaScript-loading");
+    divElement.classList.add("javaScript-loaded");
+  }, 1000);
+
+  // Return the created <div> element
+  return divElement;
+}
+
+// Get the container element with ID "javaScriptContainer"
+const container = document.getElementById("javaScriptContainer");
+
+// Array of texts for the "javaScript" elements
+const texts = [
+  "Js",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+  "Dynamic",
+  "JavaScript",
+];
+
+// Create and append "javaScript" div elements to the container
+texts.forEach((text) => {
+  const javaScriptElement = createJavaScriptElement(text);
+  container.appendChild(javaScriptElement);
+});
